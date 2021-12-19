eece5bac1a4fd553111f2422dc46c93732c447d5
tothi <tothi@users.noreply.github.com>
2021-12-19 04:54:48 +0100

    initial commit
    Signed-off-by: tothi <tothi@users.noreply.github.com>

---
+++ src/main/java/dvl4wa/VulnServlet.java
@@ -0,0 +1,32 @@
+package dvl4wa;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.io.Writer;
+import java.util.Map;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class VulnServlet extends HttpServlet {
+  protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException {
+    Logger logger = LogManager.getLogger();
+    try {
+      Map<String, String> headers = Collections.list(req.getHeaderNames()).stream().collect(Collectors.toMap(h -> h, req::getHeader));
+      res.setContentType("text/plain; charset=utf-8");
+      Writer writer = res.getWriter();
+      if(headers.containsKey("x-log")) {
+        writer.write("Logging to console using vulnerable log4j2!\n");
+        logger.info(headers.get("x-log"));
+      } else {
+        writer.write("Hello world\n");
+      }
+      writer.close();
+    } catch(Exception e) {
+      throw new ServletException(e.getMessage(), e);
+    }
+  }
+}
+
